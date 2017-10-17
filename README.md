b1185405513572d078dfe0d24ec7589e17538c39
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2017-10-17 10:13:15 +0300

    Change license to LGPL + more
    Change license to LGPL, update dependencies and update README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SRU client library and command-line interface
+# SRU client library and command-line interface [![NPM Version](https://img.shields.io/npm/v/@natlibfi/sru-client.svg)](https://npmjs.org/package/sru-client) [![Build Status](https://travis-ci.org/NatLibFi/sru-client.svg)](https://travis-ci.org/NatLibFi/sru-client) [![Test Coverage](https://codeclimate.com/github/NatLibFi/sru-client/badges/coverage.svg)](https://codeclimate.com/github/NatLibFi/sru-client/coverage)
 
 SRU (Search/Retrieval via URL) Javascript client library and command-line interface
 
@@ -22,6 +22,7 @@ TODO
 
 ## License and copyright
 
-Copyright (c) 2015 University Of Helsinki (The National Library Of Finland)
+Copyright (c) 2015,2017 University Of Helsinki (The National Library Of Finland)
 
-This project's source code is licensed under the terms of **GNU Affero General Public License Version 3**.
+This project's source code is licensed under the terms of **GNU Lesser General Public License Version 3** or any later version.
+
--- test/main.spec.js
+++ test/main.spec.js
@@ -4,28 +4,27 @@
  *
  * SRU (Search/Retrieval via URL) Javascript client library and command-line interface
  *
- * Copyright (c) 2015 University Of Helsinki (The National Library Of Finland)
+ * Copyright (c) 2015,2017 University Of Helsinki (The National Library Of Finland)
  *
  * This file is part of sru-client
  *
  * sru-client is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *  
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *  
- * You should have received a copy of the GNU Affero General Public License
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @licend  The above is the entire license notice
- * for the JavaScript code in this file.
+ * for the JavaScript code in this page.
  *
  **/
-
 (function (root, factory) {
 
     'use strict';
