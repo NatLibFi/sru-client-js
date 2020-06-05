adf6208a5e65db85e82889cacfabd7a1cec25e8e
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-06-05 11:25:05 +0300

    Add new implementation

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@
 *
 * @licstart  The following is the entire license notice for the JavaScript code in this file.
 *
-* SRU client for Node.js and browser
+* SRU Javascript client library
 *
-* Copyright (C) 2015, 2017-2018 University Of Helsinki (The National Library Of Finland)
+* Copyright (C) 2015, 2017-2018, 2020 University Of Helsinki (The National Library Of Finland)
 *
-* This file is part of sru-client
+* This file is part of sru-client-js
 *
-* sru-client program is free software: you can redistribute it and/or modify
+* sru-client-js program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Lesser General Public License as
 * published by the Free Software Foundation, either version 3 of the
 * License, or (at your option) any later version.
 *
-* sru-client is distributed in the hope that it will be useful,
+* sru-client-js is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU Lesser General Public License for more details.
@@ -26,80 +26,168 @@
 *
 */
 
-// eslint-disable max-depth
-
-import {EventEmitter} from 'events';
 import fetch from 'node-fetch';
-import {DOMParser, XMLSerializer} from 'xmldom';
+import httpStatus from 'http-status';
+import {EventEmitter} from 'events';
+import {Parser as XMLParser, Builder as XMLBuilder} from 'xml2js';
+import createDebugLogger from 'debug';
 
-const DEFAULT_VERSION = '1.0';
-const MAXIMUM_RECORDS = '1000';
-const RECORD_SCHEMA = 'marcxml';
+export const recordFormats = {
+  object: 'object',
+  string: 'string'
+};
 
-class SearchRetrieveEmitter extends EventEmitter {}
+export default ({
+  url: baseUrl,
+  recordSchema: recordSchemaDefault,
+  version = '2.0',
+  maxRecordsPerRequest = 1000,
+  recordFormat = recordFormats.string,
+  retrieveAll = true
+}) => {
+  const debug = createDebugLogger('@natlibfi/sru-client');
+  const formatRecord = createFormatter();
 
-export default function ({serverUrl, version, maximumRecords, recordSchema}) {
-	version = version || DEFAULT_VERSION;
-	maximumRecords = maximumRecords || MAXIMUM_RECORDS;
-	recordSchema = recordSchema || RECORD_SCHEMA;
+  class Emitter extends EventEmitter {
+    constructor(...args) {
+      super(args);
+    }
+  }
 
-	return {
-		searchRetrieve
-	};
+  return {searchRetrieve};
 
-	function searchRetrieve(args) {
-		const {query, offset} = parseArgs();
-		const Emitter = new SearchRetrieveEmitter();
+  function searchRetrieve(query, {startRecord = 1, recordSchema: recordSchemaArg} = {}) {
+    const recordSchema = recordSchemaArg || recordSchemaDefault;
+    const emitter = new Emitter();
 
-		pump(offset);
-		return Emitter;
+    iterate(startRecord);
+    return emitter;
 
-		function parseArgs() {
-			const {query, offset} = typeof args === 'string' ? {query: args} : args;
+    async function iterate(startRecord) {
+      try {
+        await processRequest(startRecord);
+      } catch (err) {
+        return emitter.emit('error', err);
+      }
 
-			return {
-				offset,
-				query: encodeURIComponent(query)
-			};
-		}
+      async function processRequest(startRecord) {
+        const url = generateUrl({operation: 'searchRetrieve', query, startRecord, recordSchema, version, maximumRecords: maxRecordsPerRequest});
+        debug(`Sending request: ${url.toString()}`);
+        const response = await fetch(url);
 
-		async function pump(startRecord = 1) {
-			try {
-				const url = `${serverUrl}?operation=searchRetrieve&version=${version}&maximumRecords=${maximumRecords}&recordSchema=${recordSchema}&startRecord=${startRecord}&query=${query}`;
-				const response = await fetch(url);
-				const serializer = new XMLSerializer();
-				const doc = new DOMParser().parseFromString(await response.text());
+        if (response.status === httpStatus.OK) {
+          const {records, error, nextRecordOffset} = await parsePayload(response);
 
-				try {
-					if (Number(doc.getElementsByTagName('zs:numberOfRecords').item(0).textContent) > 0) {
-						const records = doc.getElementsByTagName('zs:record');
+          if (error) { // eslint-disable-line functional/no-conditional-statement
+            throw new Error(error);
+          }
 
-						for (let i = 0; i < records.length; i++) {
-							const record = records.item(i);
+          if (records) {
+            emitRecords(records);
 
-							for (let k = 0; k < record.childNodes.length; k++) {
-								const childNode = record.childNodes.item(k);
+            debug(nextRecordOffset);
 
-								if (childNode.localName === 'recordData') {
-									Emitter.emit('record', serializer.serializeToString(childNode));
-								}
-							}
-						}
+            if (typeof nextRecordOffset === 'number') {
+              if (retrieveAll) {
+                return iterate(nextRecordOffset);
+              }
 
-						if (doc.getElementsByTagName('zs:nextRecordPosition').length > 0) {
-							pump(Number(doc.getElementsByTagName('zs:nextRecordPosition').textContent));
-						} else {
-							Emitter.emit('end');
-						}
-					} else {
-						Emitter.emit('end');
-					}
-				} catch (err) {
-					Emitter.emit('error', err);
-				}
-			} catch (err) {
-				Emitter.emit('error', err);
-			}
-		}
-	}
-}
+              return emitter.emit('end', nextRecordOffset);
+            }
+
+            return emitter.emit('end');
+          }
+
+
+          return emitter.emit('end');
+        }
+
+        throw new Error(`Unexpected response ${response.status}: ${await response.text()}`);
+
+        async function parsePayload(response) {
+          const payload = await parse();
+          const [error] = payload['zs:searchRetrieveResponse']?.['zs:diagnostics']?.[0]?.['diag:diagnostic']?.[0]?.['diag:message'] || [];
+
+          if (error) {
+            return {error};
+          }
+
+          const totalNumberOfRecords = Number(payload['zs:searchRetrieveResponse']['zs:numberOfRecords'][0]);
+
+          if (totalNumberOfRecords === 0) {
+            return {};
+          }
+
+          const records = payload['zs:searchRetrieveResponse']['zs:records'][0]['zs:record'];
+          const lastOffset = Number(records.slice(-1)[0]['zs:recordPosition'][0]);
+
+          if (lastOffset === totalNumberOfRecords) {
+            return {records};
+          }
+
+          debug(`${lastOffset}.${totalNumberOfRecords}`);
+
+          return {records, nextRecordOffset: lastOffset + 1};
+
+          async function parse() {
+            const payload = await response.text();
+
+            return new Promise((resolve, reject) => {
+              new XMLParser().parseString(payload, (err, obj) => {
+                if (err) {
+                  return reject(new Error(`Error parsing XML: ${err}, input: ${payload}`));
+                }
+                return resolve(obj);
+              });
+            });
+          }
+        }
+
+        function emitRecords(records) {
+          const [record] = records;
+
+          if (record) {
+            emitter.emit('record', formatRecord(record['zs:recordData'][0]));
+            return emitRecords(records.slice(1));
+          }
+        }
+
+        function generateUrl(params) {
+          const formatted = Object.entries(params)
+            .filter(([, value]) => value)
+            .reduce((acc, [key, value]) => ({...acc, [key]: value}), {});
+
+          const searchParams = new URLSearchParams(formatted);
+          return `${baseUrl}?${searchParams.toString()}`;
+        }
+      }
+    }
+  }
+
+  function createFormatter() {
+    if (recordFormat === recordFormats.object) {
+      return data => data;
+    }
+
+    if (recordFormat === recordFormats.string) {
+      const builder = new XMLBuilder({
+        xmldec: {
+          version: '1.0',
+          encoding: 'UTF-8',
+          standalone: false
+        },
+        renderOpts: {
+          pretty: true,
+          indent: '\t'
+        }
+      });
+
+      return data => {
+        const [[key, value]] = Object.entries(data);
+        return builder.buildObject({[key]: value[0]});
+      };
+    }
+
+    throw new Error(`Invalid record format: ${recordFormat}`);
+  }
+};
